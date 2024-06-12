42930e80714891edaf5e9c09d8dda77f26c9c111
Gavin Hunsche <gh452@cornell.edu>
2024-06-12 13:17:29 -0400

    Third Commit

--- src/low_level/offset_particle.jl
+++ src/low_level/offset_particle.jl
@@ -1,1 +1,33 @@
-include("src/structures.jl")
+include("structures.jl")
+
+function offset_particle_set!(x_offset, y_offset, tilt, p_lab)
+    """Transform from lab to element coords.
+    See Bmad manual (2022-11-06) sections 5.6.1, 15.3.1 and 24.2
+    **NOTE**: transverse only as of now.
+    """
+    
+    (x, px, y, py, z, pz) = p_lab.r
+    
+    s = sin(tilt)
+    c = cos(tilt)
+    x_ele_int = p_lab.r[1] - x_offset
+    y_ele_int = p_lab.r[3] - y_offset
+    x_ele = x_ele_int*c + y_ele_int*s
+    y_ele = -x_ele_int*s + y_ele_int*c
+
+    p_lab.r[1], p_lab.r[3] = x_ele, y_ele
+
+    px_ele = p_lab.r[2]*c + p_lab.r[4]*s
+    py_ele = -p_lab.r[2]*s + p_lab.r[4]*c
+
+    p_ele = p_lab
+    p_ele.r = (x_ele, px_ele, y_ele, py_ele, z, pz)
+
+return p_ele
+
+end
+
+
+"""Test Value"""
+p_lab = Particle(1.0, 1.0, 1.0, [1.0, 0.5, 1.0, 0.2, 1.0, 0.5])
+offset_particle_set!(0.01, 0.01, 0.01, p_lab)
---
+++ src/track_a_drift.jl
@@ -0,0 +1,40 @@
+include("/home/gh452/.julia/dev/BeamTracking/src/low_level/structures.jl")
+include("/home/gh452/.julia/dev/BeamTracking/src/low_level/sqrt_one.jl")
+
+
+function track_a_drift(p_in, drift)
+    """Tracks the incoming Particle p_in though drift element
+    and returns the outgoing particle. 
+    See Bmad manual section 24.9 
+    """
+    L = drift.L
+    
+    s = p_in.s
+    p0c = p_in.p0c
+    mc2 = p_in.mc2
+    
+    (x, px, y, py, z, pz) = p_in.r
+    
+    P = 1 + pz            # Particle's total momentum over p0
+    Px = px / P           # Particle's 'x' momentum over p0
+    Py = py / P           # Particle's 'y' momentum over p0
+    Pxy2 = Px^2 + Py^2  # Particle's transverse mometum^2 over p0^2
+    Pl = sqrt(1-Pxy2)     # Particle's longitudinal momentum over p0
+    
+    p_in.r[1] = x + L * Px / Pl
+    p_in.r[3] = y + L * Py / Pl
+    
+    # z = z + L * ( beta/beta_ref - 1.0/Pl ) but numerically accurate:
+    dz = L * (sqrt_one((mc2^2 * (2*pz+pz^2))/((p0c*P)^2 + mc2^2))
+                + sqrt_one(-Pxy2)/Pl)
+    p_in.r[5] = z + dz
+    p_in.s = s + L
+
+    return Particle = p_in
+
+end
+
+p_in = Particle(1.0, 1.0, 1.0, [1.0, 0.5, 1.0, 0.2, 1.0, 0.5])
+drift = Drift(0.001)
+
+track_a_drift(p_in, drift)
