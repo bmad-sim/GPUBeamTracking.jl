dfa26a45eca2cea4612345d90631a0e2c49a6ced
Gavin Hunsche <gh452@cornell.edu>
2024-06-25 09:44:43 -0400

    rm CPU version and cleaned up GPU version

--- src/track_a_drift_gpu.jl
+++ src/track_a_drift_gpu.jl
@@ -2,49 +2,10 @@ using CUDA, BenchmarkTools, Adapt
 include("low_level/sqrt_one.jl")
 include("low_level/structures.jl")
 
+"""Adapting structures to bitstype"""
 Adapt.@adapt_structure GPU_Particle
 Adapt.@adapt_structure GPU_Drift
 Adapt.@adapt_structure Intermediate
-    
-function track_a_drift_gpu!(p_in, drift, inter)
-    """Tracks the incoming Particle p_in though drift element
-    and returns the outgoing particle. 
-    See Bmad manual section 24.9 
-    """
-    L = drift.L
-    s = p_in.s
-    p0c = p_in.p0c
-    mc2 = p_in.mc2
-
-    x, px, y, py, z, pz = p_in.x, p_in.px, p_in.y, p_in.py, p_in.z, p_in.pz
-    P, Px, Py, Pxy2, Pl, X, Y, dz, dz1, dz2, dz3 = inter.P, inter.Px, inter.Py, 
-    inter.Pxy2, inter.Pl, inter.X, inter.Y, inter.dz, inter.dz1, inter.dz2, inter.dz3
-    
-    @inbounds P .= pz .+ 1
-    @inbounds Px .= px ./ P
-    @inbounds Py .= py ./ P
-    @inbounds Pxy2 .= Px.^2 .+ Py.^2
-    @inbounds Pl .= sqrt.(1 .- Pxy2)
-
-    @inbounds X .= L .* Px; Y .= L .* Py
-    @inbounds X ./= Pl; Y ./= Pl
-    @inbounds x .+= X; y .+= Y
-
-    # z = z + L * ( beta/beta_ref - 1.0/Pl ) but numerically accurate:
-
-
-    @inbounds dz1 .= 2 .*pz .+ pz.^2; dz1 .*= mc2.^2
-    @inbounds dz2 .= p0c.*P; dz2 .^= 2; dz2 .+= mc2.^2
-    @inbounds dz3 .= sqrt_one.(.-Pxy2); dz3 ./= Pl
-    @inbounds dz .= dz1 ./ dz2
-    @inbounds dz .= sqrt_one.(dz); dz .+= dz3; dz .*= L
-
-    @inbounds z .+= dz; s .+= L
-    
-    
-
-    return
-end
 
 """helper arrays to determine array memory allocation"""
 P = CUDA.fill(0.0, 1000); Px = CUDA.fill(0.0, 1000); Py = CUDA.fill(0.0, 1000);
@@ -57,20 +18,9 @@ s = CUDA.fill(1.0, 1000); p0c = CUDA.fill(1.0, 1000); mc2 = CUDA.fill(.511, 1000
 
 L = CUDA.fill(0.005, 1000);
 
-
 drift = GPU_Drift(L)
 inter = Intermediate(P, Px, Py, Pxy2, Pl, dz)
 p_in = GPU_Particle(x, px, y, py, z, pz, s, p0c, mc2)
-
-function bench_gpu1!(p_in, drift, inter)
-    CUDA.@sync begin
-    @cuda track_a_drift_gpu!(p_in, drift, inter)
-    end
-end
-
-@btime bench_gpu1!($p_in, $drift, $inter)
-bench_gpu1!(p_in, drift, inter)    # 11.407 ms, 11.437 ms
-CUDA.@profile bench_gpu1!(p_in, drift, inter)
 
 function track_a_drift_gpu2!(p_in, drift, inter)
     """Tracks the incoming Particle p_in though drift element
@@ -106,17 +56,15 @@ function track_a_drift_gpu2!(p_in, drift, inter)
 
         @inbounds (z[i] += dz[i]; 
                 s[i] += L[i];)
-
     end
-
     
-    
-
     return
 end
 
-@cuda threads=512 track_a_drift_gpu2!(p_in, drift, inter)
+"""testing output"""
+@cuda threads=500 track_a_drift_gpu2!(p_in, drift, inter)
 
+"""benchmarking"""
 function bench_gpu2!(p_in, drift, inter)
     CUDA.@sync begin
     @cuda threads = 500 track_a_drift_gpu2!(p_in, drift, inter)
