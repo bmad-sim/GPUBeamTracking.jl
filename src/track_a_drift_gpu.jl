55ec5268ddaf7a72236ccda69b31b1884cb6dc25
Gavin Hunsche <gh452@cornell.edu>
2024-06-24 22:39:03 -0400

    6/24/24 5th save

--- src/track_a_drift_gpu.jl
+++ src/track_a_drift_gpu.jl
@@ -71,5 +71,62 @@ function bench_gpu1!(p_in, drift, inter)
 end
 
 @btime bench_gpu1!($p_in, $drift, $inter)
-bench_gpu1!(p_in, drift, inter)
+bench_gpu1!(p_in, drift, inter)    # 11.407 ms, 11.437 ms
 CUDA.@profile bench_gpu1!(p_in, drift, inter)
+
+function track_a_drift_gpu2!(p_in, drift, inter)
+    """Tracks the incoming Particle p_in though drift element
+    and returns the outgoing particle. 
+    See Bmad manual section 24.9 
+    """
+    L = drift.L
+    s = p_in.s
+    p0c = p_in.p0c
+    mc2 = p_in.mc2
+
+    x, px, y, py, z, pz = p_in.x, p_in.px, p_in.y, p_in.py, p_in.z, p_in.pz
+    P, Px, Py, Pxy2, Pl, X, Y, dz, dz1, dz2, dz3 = inter.P, inter.Px, inter.Py, 
+    inter.Pxy2, inter.Pl, inter.X, inter.Y, inter.dz, inter.dz1, inter.dz2, inter.dz3
+    
+    index = threadIdx().x
+    stride = blockDim().x
+
+    for i=index:stride:length(x)
+        @inbounds P[i] = pz[i] + 1
+        @inbounds Px[i] = px[i] / P[i]
+        @inbounds Py[i] = py[i] / P[i]
+        @inbounds Pxy2[i] = Px[i]^2 + Py[i]^2
+        @inbounds Pl[i] = sqrt(1 - Pxy2[i])
+
+        @inbounds X[i]= L[i]* Px[i]; Y[i]= L[i]* Py[i]
+        @inbounds X[i]/= Pl[i]; Y[i]/= Pl[i]
+        @inbounds x[i] += X[i]; y[i] += Y[i]
+
+        # z = z + L * ( beta/beta_ref - 1.0/Pl ) but numerically accurate:
+
+
+        @inbounds dz1[i] = 2 *pz[i] + pz[i]^2; dz1[i] *= mc2[i]^2
+        @inbounds dz2[i] = p0c[i]*P[i]; dz2[i] ^= 2; dz2[i] += mc2[i]^2
+        @inbounds dz3[i] = sqrt_one(-Pxy2[i]); dz3[i] /= Pl[i]
+        @inbounds dz[i] = dz1[i] / dz2[i]
+        @inbounds dz[i] = sqrt_one(dz[i]); dz[i] += dz3[i]; dz[i]*= L[i]
+
+        @inbounds z[i] += dz[i]; s[i] += L[i]
+
+    end
+
+    
+    
+
+    return
+end
+
+@cuda threads=512 track_a_drift_gpu2!(p_in, drift, inter)
+
+function bench_gpu2!(p_in, drift, inter)
+    CUDA.@sync begin
+    @cuda threads = 500 track_a_drift_gpu2!(p_in, drift, inter)
+    end
+end
+
+@btime bench_gpu2!($p_in, $drift, $inter)  # 117.152 μs, 117.384 μs
