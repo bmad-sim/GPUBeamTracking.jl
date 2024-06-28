20f32f45b5fadc0a10c09d0060362d31083f92dd
Gavin Hunsche <gh452@cornell.edu>
2024-06-28 17:33:33 -0400

    cleaning up and adding comments

--- src/track_a_drift_gpu.jl
+++ src/track_a_drift_gpu.jl
@@ -7,9 +7,8 @@ Adapt.@adapt_structure GPU_Particle; Adapt.@adapt_structure GPU_Drift;
 Adapt.@adapt_structure Intermediate;
 
 function track_a_drift_gpu!(p_in, drift, inter)
-"""Tracks the incoming Particle p_in though drift element
-and returns the outgoing particle. 
-See Bmad manual section 24.9 
+"""Tracks incoming Particles p_in  on a GPU through 
+drift elements. See Bmad manual section 24.9 
 """
     L = drift.L
     s = p_in.s
@@ -19,41 +18,33 @@ See Bmad manual section 24.9
     x, px, y, py, z, pz = p_in.x, p_in.px, p_in.y, p_in.py, p_in.z, p_in.pz
     P, Px, Py, Pxy2, Pl, dz = inter.P, inter.Px, inter.Py, inter.Pxy2, inter.Pl, inter.dz
 
-    index = (blockIdx().x - Int32(1)) * blockDim().x + threadIdx().x
-    stride = gridDim().x * blockDim().x
+    index = (blockIdx().x - Int32(1)) * blockDim().x + threadIdx().x   # thread index
+    stride = gridDim().x * blockDim().x                                # total number of threads on grid
     
     i = index
     while i <= length(x)
 
-        @inbounds (P[i] = pz[i] + 1;
-        Px[i] = px[i] / P[i];
-        Py[i] = py[i] / P[i];
-        Pxy2[i] = Px[i]^2 + Py[i]^2;
-        Pl[i] = sqrt(1 - Pxy2[i]);)
+        @inbounds (P[i] = pz[i] + 1;   # CuArray of each Particle's total momentum over p0
+        Px[i] = px[i] / P[i];          # CuArray of each Particle's 'x' momentum over p0
+        Py[i] = py[i] / P[i];          # CuArray of each Particle's 'y' momentum over p0
+        Pxy2[i] = Px[i]^2 + Py[i]^2;   # CuArray of each Particle's transverse momentum^2 over p0^2
+        Pl[i] = sqrt(1 - Pxy2[i]);     # CuArray of each Particle's longitudinal momentum over p0
 
-        @inbounds (x[i] += L[i] * Px[i] / Pl[i]; 
-        y[i] += L[i] * Py[i] / Pl[i];)
+        x[i] += L[i] * Px[i] / Pl[i]; 
+        y[i] += L[i] * Py[i] / Pl[i];
 
         # z = z + L * ( beta/beta_ref - 1.0/Pl ) but numerically accurate:
-        @inbounds (dz[i] = L[i] * (sqrt_one((mc2[i]^2 * (2 *pz[i]+pz[i]^2))/((p0c[i]*P[i])^2 + mc2[i]^2))
-        + sqrt_one(-Pxy2[i])/Pl[i]);)
+        dz[i] = L[i] * (sqrt_one((mc2[i]^2 * (2 *pz[i]+pz[i]^2))/((p0c[i]*P[i])^2 + mc2[i]^2))
+        + sqrt_one(-Pxy2[i])/Pl[i]);
         
-        @inbounds (z[i] += dz[i];
+        z[i] += dz[i];
         s[i] += L[i];)
 
         i += stride;
     end
+    
     return
+
 end
 
-"""configuring number of threads and blocks"""
-function bench_gpu!(p_in, drift, inter)
-    kernel = @cuda launch=false track_a_drift_gpu!(p_in, drift, inter)
-    config = launch_configuration(kernel.fun)
-    threads = min(length(x), config.threads)
-    blocks = cld(length(x), threads)
 
-    CUDA.@sync begin
-        kernel(p_in, drift, inter; threads, blocks)
-    end
-end
