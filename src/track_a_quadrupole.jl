710231041226de5118f52868f93327a289a5b759
Gavin Hunsche <gh452@cornell.edu>
2024-07-18 12:25:10 -0400

    deleting sample inputs

--- src/track_a_quadrupole.jl
+++ src/track_a_quadrupole.jl
@@ -30,20 +30,5 @@ function track_a_quadrupole(p_in, quad)
     return nothing
 end
 
-x = CUDA.fill(1.0, 10_000); px = CUDA.fill(0.8, 10_000); y = CUDA.fill(1.0, 10_000);
-py = CUDA.fill(0.85, 10_000); z = CUDA.fill(0.5, 10_000); pz = CUDA.fill(0.2, 10_000);
-s = 1.0; p0c = CUDA.fill(1.184, 10_000); mc2 = 0.511;
-
-NUM_STEPS = Int32(1000); K1 = 1.0; L = 0.5;
-TILT = CUDA.fill(1.0, 10_000); X_OFFSET = CUDA.fill(0.006, 10_000); Y_OFFSET = CUDA.fill(0.01, 10_000);
-
-x_ele_int = CUDA.fill(0.0, 10_000); x_ele = CUDA.fill(0.0, 10_000); y_ele = CUDA.fill(0.0, 10_000);
-px_ele = CUDA.fill(0.0, 10_000); py_ele = CUDA.fill(0.0, 10_000); S = CUDA.fill(0.0, 10_000); C = CUDA.fill(0.0, 10_000);
-
-p_in = particle(x, px, y, px, z, pz, s, p0c, mc2);
-quad = quad_input(L, K1, NUM_STEPS, X_OFFSET, Y_OFFSET, TILT);
-int = int_set(x_ele_int, x_ele, y_ele, px_ele, py_ele, S, C);
-
-kernel = @cuda launch=false track_a_quadrupole(p_in, quad)
 
 
